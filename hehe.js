aea196447790d62632f1416f4da4e1dbcbf79a80
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-06 02:13:50 -0400

    Update hehe.js

--- hehe.js
+++ hehe.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./botconfig.json');
-const request = require('pornsearch');
+const Pornsearch = require('pornsearch');
+const Searcher = new Pornsearch(query, driver = 'pornhub');
 const { prefix, token } = require('./botconfig.json');
 
 client.on(`ready`, () => {
@@ -9,5 +10,7 @@ client.on(`ready`, () => {
 client.user.setActivity('Pornhub Games 2020');
 
  });   
+
+
      
 client.login(process.env.BOT_TOKEN);
