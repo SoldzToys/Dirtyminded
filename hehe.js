adf95411fb74f0f7225a96769d300c64129dea85
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-07 04:04:38 -0400

    Update hehe.js

--- hehe.js
+++ hehe.js
@@ -3,12 +3,49 @@ const client = new Discord.Client();
 const config = require('./botconfig.json');
 const { prefix, token } = require('./botconfig.json');
 const Pornsearch = require('pornsearch');
-const PornDriver = Pornsearch.search('amateur');
+const Searcher = new Pornsearch('tits');
 
 client.on(`ready`, () => {
   console.log(`I'm about to get on!`);
    client.user.setActivity("Porn", {type: "WATCHING"});
   client.user.setStatus("idle")
+  
+});
+
+client.on('message', message => {
+  if (message.channel.type == "text") {
+    if (message.author.bot === false) {
+      if (message.channel.nsfw == true) {
+        if (message.content.split(' ')[0] == "-porn") {
+          var search = message.content.substr(6);
+          if (search == "") {
+            message.channel.send("Usage: -porn [RECHERCHE]")
+          }
+          const Searcher = new Pornsearch(search);
+          Searcher.videos().then(function(videos) {
+            var result = videos;
+            var result_count = result.length;
+            var rand = Math.floor(Math.random() * result_count);
+            var result_solo = videos[rand];
+            message.channel.send('Here is an image X responding to the search "' + search + '"', {
+              embed: {
+                image: {
+                  url: result_solo['thumb']
+                },
+                url: result_solo['url'],
+                title: result_solo['title'],
+                description: "duration: " + result_solo['duration']
+              }
+            });
+          });
+
+        }
+      } else {
+        message.channel.send("⛔ The 'NSFW' option must be enabled! Go to the current settings to activate it! ⛔")
+      }
+    }
+  }
+})
   
 
  });   
